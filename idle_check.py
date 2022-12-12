1ebf157b347ce4ebb81d96b36ec2b9b1557f9319
aribrilliantsyah <ariardiansyah101@gmail.com>
2022-12-12 11:51:32 +0700

    FROM EMAIL

--- idle_check.py
+++ idle_check.py
@@ -1,13 +1,20 @@
-import time, schedule, pywinauto
+import time, schedule, pywinauto, pyautogui
 from localStoragePy import localStoragePy
-from ctypes import Structure,windll,c_uint,sizeof,byref
+from sys import platform
+from idle_time import IdleMonitor
 
-localStorage = localStoragePy('idle-windows', 'json')
+localStorage = localStoragePy('idle-windows', 'sqlite')
 def idletime():
-    class LASTINPUTINFO(Structure):
-        _fields_ = [('cbSize', c_uint), ('dwTime', c_uint)]
+
+    if platform == "win32":
+        from ctypes import Structure,c_uint,sizeof,byref
+        class LASTINPUTINFO(Structure):
+            _fields_ = [('cbSize', c_uint), ('dwTime', c_uint)]
     
-    def get_idle_duration():
+    def for_win():
+        from ctypes import windll
+        print(f"WINDOWS")
+
         lastinputinfo = LASTINPUTINFO()
         lastinputinfo.cbSize = sizeof(lastinputinfo)
         
@@ -21,25 +28,68 @@ def idletime():
 
         print(f"Before: {b_total_idle}")
 
-        total_idle = float(b_total_idle) + idle;
+        total_idle = float(b_total_idle) + idle
         localStorage.setItem("total_idle", total_idle)
 
         d = dict()
         d["cuur_idle"] = idle
         d["total_idle"] = total_idle
         return d
-
-    objidle = get_idle_duration()
-    total_idle = objidle.get("total_idle")
-    curr_idle = objidle.get("cuur_idle")
     
-    print(f"Current idle in seconds: {curr_idle}")
-    print(f"Total idle in seconds: {total_idle}")
+    def for_osx():
+        d = dict()
+        d["cuur_idle"] = 0
+        d["total_idle"] = 0
+        return d
 
-    if curr_idle > 20.0:
-        pywinauto.mouse.move(coords=(100, 10))
-        pywinauto.mouse.move(coords=(100, 40))
-        pywinauto.mouse.move(coords=(100, 10))
+    def for_linux():
+        print(f"LINUX")
+
+        monitor = IdleMonitor.get_monitor()
+        idle = monitor.get_idle_time()
+        curr_idle = idle
+        b_total_idle = localStorage.getItem("total_idle")
+        
+        if b_total_idle is None:
+            b_total_idle = 0
+
+        print(f"Before: {b_total_idle}")
+
+        total_idle = float(b_total_idle) + idle
+        localStorage.setItem("total_idle", total_idle)
+        
+        d = dict()
+        d["cuur_idle"] = curr_idle
+        d["total_idle"] = total_idle
+        return d
+
+    if platform == "linux" or platform == "linux2":
+        objidle = for_linux()
+    elif platform == "darwin":
+        objidle = for_osx()
+    elif platform == "win32":
+        objidle = for_win()
+    else:
+        objidle = None
+
+    if objidle is not None: 
+        total_idle = objidle.get("total_idle")
+        curr_idle = objidle.get("cuur_idle")
+        
+        print(f"Current idle in seconds: {curr_idle}")
+        print(f"Total idle in seconds: {total_idle}")
+
+        if curr_idle > 20.0:
+            if platform == "win32":
+                print("WIN - Move Cursor")
+                pywinauto.mouse.move(coords=(100, 10))
+                pywinauto.mouse.move(coords=(100, 40))
+                pywinauto.mouse.move(coords=(100, 10))
+            elif platform == "linux" or platform == "linux2":
+                print("LINUX - Move Cursor")
+                pyautogui.moveTo(100, 500)
+                pyautogui.moveTo(100, 100)
+                pyautogui.moveTo(100, 500)
 
 def listen(): 
     idletime()
